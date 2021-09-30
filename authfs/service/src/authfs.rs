3189af08bfd7f3738826bfdd45877fe68658975b
Alan Stokes <alanstokes@google.com>
2021-09-30 17:51:19 +0100

    Extract a library for common binder-related things
    Initially this just unifies the various copies of we have of
    new_binder_exception(), and adds a variation for service-specific
    errors. (I thought this would help me solve my problem with missing
    error info, but it turns out I was wrong.) I'm intending to move more
    things here though to facilitate reuse.
    
    Bug: 186126194
    Test: atest -p
    Change-Id: I82187903b55c4cd64307065761e1e03c4e6012f4

--- authfs/service/src/authfs.rs
+++ authfs/service/src/authfs.rs
@@ -26,7 +26,6 @@ use std::process::Command;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use crate::common::new_binder_exception;
 use authfs_aidl_interface::aidl::com::android::virt::fs::IAuthFs::{BnAuthFs, IAuthFs};
 use authfs_aidl_interface::aidl::com::android::virt::fs::{
     AuthFsConfig::AuthFsConfig, InputFdAnnotation::InputFdAnnotation,
@@ -35,6 +34,7 @@ use authfs_aidl_interface::aidl::com::android::virt::fs::{
 use authfs_aidl_interface::binder::{
     self, BinderFeatures, ExceptionCode, Interface, ParcelFileDescriptor, Strong,
 };
+use binder_common::new_binder_exception;
 
 const AUTHFS_BIN: &str = "/system/bin/authfs";
 const AUTHFS_SETUP_POLL_INTERVAL_MS: Duration = Duration::from_millis(50);
