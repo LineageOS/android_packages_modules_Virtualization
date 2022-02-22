a6108317413916f745a4ed9aca3acb7624595051
Jooyung Han <jooyung@google.com>
2022-02-22 18:53:18 +0900

    Refactor: pull up getPathForPackage()
    This also fixes a runtime dependency problem by changing the module type
    from java_test_helper_library to plain java_library_host. Previously, I
    had a problem that after adding a method to the base class calling it
    resulted in NoMethodFoundException at runtime (when running atest).
    
    Bug: n/a
    Test: atest MicrodroidHostTestCases
    Change-Id: I8430bc6f2adf43a62bc7b91fd0a3fb26d5c65807

--- tests/hostside/helper/java/android/virt/test/VirtualizationTestCaseBase.java
+++ tests/hostside/helper/java/android/virt/test/VirtualizationTestCaseBase.java
@@ -194,6 +194,22 @@ public abstract class VirtualizationTestCaseBase extends BaseHostJUnit4Test {
         }
     }
 
+    public String getPathForPackage(String packageName)
+            throws DeviceNotAvailableException {
+        return getPathForPackage(getDevice(), packageName);
+    }
+
+    // Get the path to the installed apk. Note that
+    // getDevice().getAppPackageInfo(...).getCodePath() doesn't work due to the incorrect
+    // parsing of the "=" character. (b/190975227). So we use the `pm path` command directly.
+    private static String getPathForPackage(ITestDevice device, String packageName)
+            throws DeviceNotAvailableException {
+        CommandRunner android = new CommandRunner(device);
+        String pathLine = android.run("pm", "path", packageName);
+        assertTrue("package not found", pathLine.startsWith("package:"));
+        return pathLine.substring("package:".length());
+    }
+
     public static String startMicrodroid(
             ITestDevice androidDevice,
             IBuildInfo buildInfo,
@@ -247,13 +263,8 @@ public abstract class VirtualizationTestCaseBase extends BaseHostJUnit4Test {
             androidDevice.installPackage(apkFile, /* reinstall */ true);
         }
 
-        // Get the path to the installed apk. Note that
-        // getDevice().getAppPackageInfo(...).getCodePath() doesn't work due to the incorrect
-        // parsing of the "=" character. (b/190975227). So we use the `pm path` command directly.
         if (apkPath == null) {
-            apkPath = android.run("pm", "path", packageName);
-            assertTrue(apkPath.startsWith("package:"));
-            apkPath = apkPath.substring("package:".length());
+            apkPath = getPathForPackage(androidDevice, packageName);
         }
 
         android.run("mkdir", "-p", TEST_ROOT);
