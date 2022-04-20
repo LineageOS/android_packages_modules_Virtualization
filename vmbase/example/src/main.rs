f7b6dc88fd29874eb2f6fe426a1aa15bda2e9a5e
Andrew Walbran <qwandor@google.com>
2022-04-20 16:24:30 +0000

    Add a heap allocator.
    Bug: 223166344
    Test: Ran unprotected VM under crosvm.
    Change-Id: Ia004b21604ed6f2d0edc7bc7c317f3de35844dcb

--- vmbase/example/src/main.rs
+++ vmbase/example/src/main.rs
@@ -16,14 +16,24 @@
 
 #![no_main]
 #![no_std]
+#![feature(default_alloc_error_handler)]
 
 mod exceptions;
 
+extern crate alloc;
+
+use alloc::{vec, vec::Vec};
+use buddy_system_allocator::LockedHeap;
 use vmbase::{main, println};
 
 static INITIALISED_DATA: [u32; 4] = [1, 2, 3, 4];
 static mut ZEROED_DATA: [u32; 10] = [0; 10];
 static mut MUTABLE_DATA: [u32; 4] = [1, 2, 3, 4];
+
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::<32>::new();
+
+static mut HEAP: [u8; 65536] = [0; 65536];
 
 main!(main);
 
@@ -32,6 +42,12 @@ pub fn main() {
     println!("Hello world");
     print_addresses();
     check_data();
+
+    unsafe {
+        HEAP_ALLOCATOR.lock().init(&mut HEAP as *mut u8 as usize, HEAP.len());
+    }
+
+    check_alloc();
 }
 
 fn print_addresses() {
@@ -81,6 +97,7 @@ fn check_data() {
     unsafe {
         println!("ZEROED_DATA: {:#010x}", &ZEROED_DATA as *const u32 as usize);
         println!("MUTABLE_DATA: {:#010x}", &MUTABLE_DATA as *const u32 as usize);
+        println!("HEAP: {:#010x}", &HEAP as *const u8 as usize);
     }
 
     assert_eq!(INITIALISED_DATA[0], 1);
@@ -105,6 +122,18 @@ fn check_data() {
         assert_eq!(MUTABLE_DATA[0], 42);
     }
     println!("Data looks good");
+}
+
+fn check_alloc() {
+    println!("Allocating a Vec...");
+    let mut vector: Vec<u32> = vec![1, 2, 3, 4];
+    assert_eq!(vector[0], 1);
+    assert_eq!(vector[1], 2);
+    assert_eq!(vector[2], 3);
+    assert_eq!(vector[3], 4);
+    vector[2] = 42;
+    assert_eq!(vector[2], 42);
+    println!("Vec seems to work.");
 }
 
 extern "C" {
