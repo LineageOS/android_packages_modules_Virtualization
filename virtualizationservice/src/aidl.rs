0f71596bd5333f08d07ed33dbb5425390dd8aa79
Shikha Panwar <shikhapanwar@google.com>
2022-09-28 11:10:04 +0000

    Revert "Boot VM without bootloader - using init ramdisk"
    This reverts commit c700b84bbe41dae84583e64c142167b74ec54d17.
    
    Test: N/A
    Bug; 249462836
    
    Change-Id: I26c96723fe5dc46c60bbb40738d5ca082726b45b

--- virtualizationservice/src/aidl.rs
+++ virtualizationservice/src/aidl.rs
@@ -632,7 +632,7 @@ fn load_app_config(
     vm_config.numCpus = config.numCpus;
     vm_config.taskProfiles = config.taskProfiles.clone();
 
-    // Microdroid requires an additional init ramdisk & payload disk image
+    // Microdroid requires an additional payload disk image and the bootconfig partition.
     if os_name == "microdroid" {
         add_microdroid_images(
             config,
