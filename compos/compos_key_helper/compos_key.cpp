7c6e4185bb7a869eaf21ba1fbee916abbb2777a1
Andrew Scull <ascull@google.com>
2022-10-07 19:47:53 +0000

    Simplify compos key handling
    The new interface to the VM instance secrets already provides a
    context-specific secret so it can be used directly as the seed for the
    key without requiring another derivation.
    
    Bug: 243514248
    Test: atest ComposHostTestCases
    Test: atest compos_key_tests
    Change-Id: Ibbff27cc6e4e8f499bf70ff6ce171f6cd7765288

--- compos/compos_key_helper/compos_key.cpp
+++ compos/compos_key_helper/compos_key.cpp
@@ -24,25 +24,11 @@ using android::base::ErrnoError;
 using android::base::Error;
 using android::base::Result;
 using compos_key::Ed25519KeyPair;
+using compos_key::Seed;
 using compos_key::Signature;
 
-// Used to ensure the key we derive is distinct from any other.
-constexpr const char* kSigningKeyInfo = "CompOS signing key";
-
 namespace compos_key {
-Result<Ed25519KeyPair> deriveKeyFromSecret(const uint8_t* secret, size_t secret_size) {
-    // Ed25519 private keys are derived from a 32 byte seed:
-    // https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.5
-    std::array<uint8_t, 32> seed;
-
-    // We derive the seed from the secret using HKDF - see
-    // https://datatracker.ietf.org/doc/html/rfc5869#section-2.
-    if (!HKDF(seed.data(), seed.size(), EVP_sha256(), secret, secret_size, /*salt=*/nullptr,
-              /*salt_len=*/0, reinterpret_cast<const uint8_t*>(kSigningKeyInfo),
-              strlen(kSigningKeyInfo))) {
-        return Error() << "HKDF failed";
-    }
-
+Result<Ed25519KeyPair> keyFromSeed(const Seed& seed) {
     Ed25519KeyPair result;
     ED25519_keypair_from_seed(result.public_key.data(), result.private_key.data(), seed.data());
     return result;
