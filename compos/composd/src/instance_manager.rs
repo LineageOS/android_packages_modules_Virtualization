39da7697f097a9d0c4baadb5fc1683b67795c6df
Alan Stokes <alanstokes@google.com>
2023-02-03 11:19:36 +0000

    Increase max VM memory for CompOS
    Since we are seeing occasional OOM failures.
    
    Bug: 264496291
    Test: atest ComposTestCase#testOdrefreshSpeedProfile
    Change-Id: I78aa72405d9e08652612d5d6375a661678e24025

--- compos/composd/src/instance_manager.rs
+++ compos/composd/src/instance_manager.rs
@@ -30,7 +30,7 @@ use std::sync::{Arc, Mutex, Weak};
 use virtualizationservice::IVirtualizationService::IVirtualizationService;
 
 // Enough memory to complete odrefresh in the VM.
-const VM_MEMORY_MIB: i32 = 1024;
+const VM_MEMORY_MIB: i32 = 1280;
 
 pub struct InstanceManager {
     service: Strong<dyn IVirtualizationService>,
