2422bdc31e7be8ce73a9b1ed1e39c2f6db925b18
Alice Wang <aliceywang@google.com>
2023-06-12 08:37:55 +0000

    [fdt] Add function to get the first range in /memory node of fdt
    and test it. This function will be reused later in both pvmfw and
    rialto.
    
    Test: atest liblibfdt.integration_test
    Test: atest vmbase_example.integration_test
    Test: m pvmfw_img
    Bug: 284462758
    Change-Id: Ic547530f911281c2db14b9a59b7e2470be10361f

--- libs/libfdt/tests/api_test.rs
+++ libs/libfdt/tests/api_test.rs
@@ -16,18 +16,57 @@
 
 //! Integration tests of the library libfdt.
 
-use libfdt::Fdt;
+use libfdt::{Fdt, FdtError};
 use std::fs;
 use std::ops::Range;
 
-const TEST_TREE1_PATH: &str = "data/test_tree1.dtb";
+const TEST_TREE_WITH_ONE_MEMORY_RANGE_PATH: &str = "data/test_tree_one_memory_range.dtb";
+const TEST_TREE_WITH_MULTIPLE_MEMORY_RANGES_PATH: &str =
+    "data/test_tree_multiple_memory_ranges.dtb";
+const TEST_TREE_WITH_EMPTY_MEMORY_RANGE_PATH: &str = "data/test_tree_empty_memory_range.dtb";
+const TEST_TREE_WITH_NO_MEMORY_NODE_PATH: &str = "data/test_tree_no_memory_node.dtb";
 
 #[test]
-fn parse_well_formed_fdt_successfully() {
-    let data = fs::read(TEST_TREE1_PATH).unwrap();
+fn retrieving_memory_from_fdt_with_one_memory_range_succeeds() {
+    let data = fs::read(TEST_TREE_WITH_ONE_MEMORY_RANGE_PATH).unwrap();
     let fdt = Fdt::from_slice(&data).unwrap();
 
     const EXPECTED_FIRST_MEMORY_RANGE: Range<usize> = 0..256;
-    let mut memory = fdt.memory().unwrap().unwrap();
+    let mut memory = fdt.memory().unwrap();
     assert_eq!(memory.next(), Some(EXPECTED_FIRST_MEMORY_RANGE));
+    assert!(memory.next().is_none());
+    assert_eq!(fdt.first_memory_range(), Ok(EXPECTED_FIRST_MEMORY_RANGE));
+}
+
+#[test]
+fn retrieving_memory_from_fdt_with_multiple_memory_ranges_succeeds() {
+    let data = fs::read(TEST_TREE_WITH_MULTIPLE_MEMORY_RANGES_PATH).unwrap();
+    let fdt = Fdt::from_slice(&data).unwrap();
+
+    const EXPECTED_FIRST_MEMORY_RANGE: Range<usize> = 0..256;
+    const EXPECTED_SECOND_MEMORY_RANGE: Range<usize> = 512..1024;
+    let mut memory = fdt.memory().unwrap();
+    assert_eq!(memory.next(), Some(EXPECTED_FIRST_MEMORY_RANGE));
+    assert_eq!(memory.next(), Some(EXPECTED_SECOND_MEMORY_RANGE));
+    assert!(memory.next().is_none());
+    assert_eq!(fdt.first_memory_range(), Ok(EXPECTED_FIRST_MEMORY_RANGE));
+}
+
+#[test]
+fn retrieving_first_memory_from_fdt_with_empty_memory_range_fails() {
+    let data = fs::read(TEST_TREE_WITH_EMPTY_MEMORY_RANGE_PATH).unwrap();
+    let fdt = Fdt::from_slice(&data).unwrap();
+
+    let mut memory = fdt.memory().unwrap();
+    assert!(memory.next().is_none());
+    assert_eq!(fdt.first_memory_range(), Err(FdtError::NotFound));
+}
+
+#[test]
+fn retrieving_memory_from_fdt_with_no_memory_node_fails() {
+    let data = fs::read(TEST_TREE_WITH_NO_MEMORY_NODE_PATH).unwrap();
+    let fdt = Fdt::from_slice(&data).unwrap();
+
+    assert_eq!(fdt.memory().unwrap_err(), FdtError::NotFound);
+    assert_eq!(fdt.first_memory_range(), Err(FdtError::NotFound));
 }
