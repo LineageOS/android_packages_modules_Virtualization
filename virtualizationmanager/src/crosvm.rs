c0611e7c224c4de4708627de08d94ae5f1b17259
Pierre-Clément Tosi <ptosi@google.com>
2023-09-15 11:41:07 +0100

    virtualizationmanager: Use str::split_once()
    Small refactor to make use of a more appropriate method.
    
    Test: TH
    Change-Id: I74366b24b8009676f4d7033ddfc43462bae4f374

--- virtualizationmanager/src/crosvm.rs
+++ virtualizationmanager/src/crosvm.rs
@@ -642,10 +642,10 @@ fn get_rss(pid: u32) -> Result<Rss> {
 }
 
 fn death_reason(result: &Result<ExitStatus, io::Error>, mut failure_reason: &str) -> DeathReason {
-    if let Some(position) = failure_reason.find('|') {
+    if let Some((reason, info)) = failure_reason.split_once('|') {
         // Separator indicates extra context information is present after the failure name.
-        error!("Failure info: {}", &failure_reason[(position + 1)..]);
-        failure_reason = &failure_reason[..position];
+        error!("Failure info: {info}");
+        failure_reason = reason;
     }
     if let Ok(status) = result {
         match failure_reason {
